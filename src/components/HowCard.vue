86eddabebeadf1e0039a589c148744ff0c325eb8
musakanneh <mkanneh17@alustudent.com>
2020-12-20 17:06:51 +0200

    Updates: What and How we do pages

--- src/components/HowCard.vue
+++ src/components/HowCard.vue
@@ -1,120 +1,195 @@
 <template>
   <body>
-    <section class="about">
-      <div class="sec-one-cover">
-        <div class="sec-one-inner">
-          <div class="image">
-            <img src="../assets/nature.jpg" alt="" />
+    <section class="activities-sec-cover">
+      <article>
+        <tabs
+          :tabs="tabs"
+          :currentTab="currentTab"
+          :wrapper-class="'default-tabs'"
+          :tab-class="'default-tabs__item'"
+          :tab-active-class="'default-tabs__item_active'"
+          :line-class="'default-tabs__active-line'"
+          @onClick="handleClick"
+        />
+        <div class="content">
+          <div v-if="currentTab === 'tab1'" class="current-tab-sec">
+            <div class="">
+              <h2>COLLABORATIVE RESEARCH</h2>
+              <p>
+                TEST shall host a collaborative research program with renowned
+                academic and research institutions to develop the database of
+                the indigenous ecological knowledge practices that enhance
+                environmental and cultural sustainability. Furthermore, explore
+                best options to merge scientific conservation approaches with
+                indigenous traditional knowledge towards sustainable
+                conservation actions that address resource tenure and
+                governance, environmental hazards, illegal wildlife trade, and
+                the climate emergency.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
           </div>
-          <h3>COLLABORATIVE RESEARCH</h3>
-          <p v-scrollanimation>
-            TEST shall host a collaborative research program with renowned
-            academic and research institutions to develop the database of the
-            indigenous ecological knowledge practices that enhance environmental
-            and cultural sustainability. Furthermore, explore best options to
-            merge scientific conservation approaches with indigenous traditional
-            knowledge towards sustainable conservation actions that address
-            resource tenure and governance, environmental hazards, illegal
-            wildlife trade, and the climate emergency.
-          </p>
-        </div>
-      </div>
-    </section>
-    <section class="about">
-      <div class="sec-one-cover">
-        <div class="sec-one-inner">
-          <div class="image">
-            <img src="../assets/nature.jpg" alt="" />
+          <div v-if="currentTab === 'tab2'" class="current-tab-sec">
+            <div>
+              <h2>EDUCATION AND CAPACITY BUILDING</h2>
+              <p>
+                TEST shall facilitate a comprehensive awareness creation program
+                to disseminate and exhibit the findings of the action research.
+                The action will employ such media as film, radio, and other
+                storytelling tools. The purpose is to enable the scientific
+                forum to recognize and adopt indigenous communities’ best land
+                management practices for sustainable ecosystems management and
+                integrate such suitable indigenous cultures into official school
+                curricula.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
           </div>
-          <h3>EDUCATION AND CAPACITY BUILDING</h3>
-          <p v-scrollanimation>
-            TEST shall facilitate a comprehensive awareness creation program to
-            disseminate and exhibit the findings of the action research. The
-            action will employ such media as film, radio, and other storytelling
-            tools. The purpose is to enable the scientific forum to recognize
-            and adopt indigenous communities’ best land management practices for
-            sustainable ecosystems management and integrate such suitable
-            indigenous cultures into official school curricula.
-          </p>
-        </div>
-      </div>
-    </section>
-
-    <section class="about">
-      <div class="sec-one-cover">
-        <div class="sec-one-inner">
-          <div class="image">
-            <img src="../assets/nature.jpg" alt="" />
+          <div v-if="currentTab === 'tab3'" class="current-tab-sec">
+            <div>
+              <h2>CULTURAL EXCHANGE</h2>
+              <p>
+                Tanzania has a diversity if indigenous cultures contributing to
+                community and ecological resilience. TEST will facilitate
+                targeted cultural exchange visits with and among diverse
+                cultural groups to enhance learning, sharing culturally valued
+                lessons across diverse communities in Tanzania and beyond.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
           </div>
-          <h3>CULTURAL EXCHANGE</h3>
-          <p v-scrollanimation>
-            Tanzania has a diversity if indigenous cultures contributing to
-            community and ecological resilience. TEST will facilitate targeted
-            cultural exchange visits with and among diverse cultural groups to
-            enhance learning, sharing culturally valued lessons across diverse
-            communities in Tanzania and beyond.
-          </p>
-        </div>
-      </div>
-    </section>
-    <section class="about">
-      <div class="sec-one-cover">
-        <div class="sec-one-inner">
-          <div class="image">
-            <img src="../assets/nature.jpg" alt="" />
+          <div v-if="currentTab === 'tab4'" class="current-tab-sec">
+            <div>
+              <h2>CULTURAL EXCHANGE</h2>
+              <p>
+                TEST shall spearhead the development of a
+                participatory-landscape approach to land-use-plans including
+                guidelines for governing and controlling the use of the land
+                resources at the community level and ecosystem-wide levels.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
           </div>
-          <h3>ECOSYSTEM GOVERNANCE</h3>
-          <p v-scrollanimation>
-            TEST shall spearhead the development of a participatory-landscape
-            approach to land-use-plans including guidelines for governing and
-            controlling the use of the land resources at the community level and
-            ecosystem-wide levels.
-          </p>
         </div>
-      </div>
+      </article>
+      <aside></aside>
     </section>
   </body>
 </template>
 
 <script>
-// import Button from "@/components/Button.vue";
+import Tabs from "vue-tabs-with-active-line";
+const TABS = [
+  {
+    title: "RESEARCH",
+    value: "tab1",
+  },
+  {
+    title: "EDUCATION",
+    value: "tab2",
+  },
+  {
+    title: "EXCHANGE",
+    value: "tab3",
+  },
+  {
+    title: "GOVERNANCE",
+    value: "tab4",
+  },
+];
 export default {
-  components: {},
+  components: {
+    Tabs,
+  },
+  data: () => ({
+    tabs: TABS,
+    currentTab: "tab1",
+  }),
+  methods: {
+    handleClick(newTab) {
+      this.currentTab = newTab;
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 body {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  width: 100%;
-  padding: 2% 0 10% 0;
+  .current-tab-sec {
+    // border: 1px solid red;
+    display: flex;
+  }
+  .activities-sec-cover {
+    display: flex;
 
-  flex: auto;
-  flex-direction: row;
+    article {
+      flex: 1;
+    }
 
-  .about {
-    justify-content: space-between;
-    width: 95%;
-    background: #ffffff;
-    margin: auto;
-    box-shadow: 0px 6px 20px rgba(0, 0, 0, 0.06);
-
-    .sec-one-inner {
+    img {
       width: 100%;
-      h3,
-      p {
-        margin: 1.5rem;
-      }
+    }
+  }
 
-      h3 {
+  .default-tabs {
+    width: 85%;
+    position: relative;
+    margin: 0 auto;
+    &__item {
+      display: inline-block;
+      margin: 0 5px;
+      padding: 10px;
+      padding-bottom: 8px;
+      font-size: 16px;
+      letter-spacing: 0.8px;
+      color: #303030;
+      text-decoration: none;
+      border: none;
+      background-color: transparent;
+      border-bottom: 2px solid transparent;
+      cursor: pointer;
+      transition: all 0.25s;
+      &_active {
+        color: black;
+      }
+      &:hover {
+        border-bottom: 2px solid #c98811;
         color: #c98811;
       }
-
-      img {
-        width: 100%;
+      &:focus {
+        outline: none;
+        border-bottom: 2px solid #c98811;
+        color: #c98811;
+      }
+      &:first-child {
+        margin-left: 0;
+      }
+      &:last-child {
+        margin-right: 0;
       }
     }
+    &__active-line {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 2px;
+      background-color: black;
+      transition: transform 0.4s ease, width 0.4s ease;
+    }
+  }
+  .content {
+    margin-top: 30px;
+    font-size: 20px;
+    width: 85%;
+    margin: auto;
   }
 }
 </style>
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,61 +1,85 @@
 <template>
-  <div>
-  <br><br><br><br>
-    <tabs
-      :tabs="tabs"
-      :currentTab="currentTab"
-      :wrapper-class="'default-tabs'"
-      :tab-class="'default-tabs__item'"
-      :tab-active-class="'default-tabs__item_active'"
-      :line-class="'default-tabs__active-line'"
-      @onClick="handleClick"
-    />
-    <div class="content">
-      <div v-if="currentTab === 'tab1'">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
-        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
-        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
-        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
-        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
-        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
-        voluptatem.
-      </div>
-      <div v-if="currentTab === 'tab2'">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </div>
-      <div v-if="currentTab === 'tab3'">
-        At vero eos et accusamus et iusto odio dignissimos ducimus qui
-        blanditiis praesentium voluptatum deleniti atque corrupti quos dolores
-        et quas molestias excepturi sint occaecati cupiditate non provident,
-        similique sunt in culpa qui officia deserunt mollitia animi, id est
-        laborum et dolorum fuga.
-      </div>
-    </div>
-  </div>
+  <body>
+    <br /><br /><br /><br /><br />
+    <section class="activities-sec-cover">
+      <article>
+        <tabs
+          :tabs="tabs"
+          :currentTab="currentTab"
+          :wrapper-class="'default-tabs'"
+          :tab-class="'default-tabs__item'"
+          :tab-active-class="'default-tabs__item_active'"
+          :line-class="'default-tabs__active-line'"
+          @onClick="handleClick"
+        />
+        <div class="content">
+          <div v-if="currentTab === 'tab1'" class="current-tab-sec">
+            <div class="">
+              <p>
+                The wildlife in natural ecosystems share their habitats with
+                indigenous peoples and their livelihoods (grazing, farms).
+                Wildlife movements within these ecosystems have been and
+                continue to experience challenges and are a source of
+                human-wildlife conflicts. Just having wildlife on these
+                ecosystems, people and wildlife face challenges of co-habiting.
+                The future of wildlife in Tanzania depends on positive
+                understanding of indigenous communities and the acceptance of
+                their role.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
+          </div>
+          <div v-if="currentTab === 'tab2'" class="current-tab-sec">
+            <div>
+              <p>
+                To re-activate the support of indigenous communities whose
+                indigenous traditional knowledge systems and their territories
+                are vital in wildlife conservation and enhance meaningful
+                collaboration of indigenous communities and government agencies.
+                Through this achievement, we will be able to enhance the quality
+                of wildlife habitats.
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
+          </div>
+          <div v-if="currentTab === 'tab3'" class="current-tab-sec">
+            <div>
+              <p>
+                TEST is committed to facilitating conservation education and
+                campaigns for indigenous communities and assist in obtaining
+                formal recognition of their indigenous community conserved areas
+                (ICCAs) for wildlife in the natural ecosystems in Tanzania. (SDG
+                15, 13, 17, etc)
+              </p>
+            </div>
+            <div>
+              <img src="../assets/lio2.jpeg" alt="" />
+            </div>
+          </div>
+        </div>
+      </article>
+      <aside></aside>
+    </section>
+  </body>
 </template>
 
 <script>
-import Tabs from 'vue-tabs-with-active-line';
-// import Tabs from "../src/VueTabsWithActiveLine.vue";
+import Tabs from "vue-tabs-with-active-line";
 const TABS = [
   {
-    title: "Tab 1",
+    title: "Why",
     value: "tab1",
   },
   {
-    title: "Tab 2",
+    title: "Objective",
     value: "tab2",
   },
   {
-    title: "Tab 3",
+    title: "Plan",
     value: "tab3",
   },
 ];
@@ -76,53 +100,74 @@ export default {
 </script>
 
 <style lang="scss">
-.default-tabs {
-  position: relative;
-  margin: 0 auto;
-  &__item {
-    display: inline-block;
-    margin: 0 5px;
-    padding: 10px;
-    padding-bottom: 8px;
-    font-size: 16px;
-    letter-spacing: 0.8px;
-    color: gray;
-    text-decoration: none;
-    border: none;
-    background-color: transparent;
-    border-bottom: 2px solid transparent;
-    cursor: pointer;
-    transition: all 0.25s;
-    &_active {
-      color: black;
+body {
+  .current-tab-sec {
+    // border: 1px solid red;
+    display: flex;
+  }
+  .activities-sec-cover {
+    display: flex;
+
+    article {
+      flex: 1;
     }
-    &:hover {
-      border-bottom: 2px solid gray;
-      color: black;
-    }
-    &:focus {
-      outline: none;
-      border-bottom: 2px solid gray;
-      color: black;
-    }
-    &:first-child {
-      margin-left: 0;
-    }
-    &:last-child {
-      margin-right: 0;
+
+    img {
+      width: 100%;
     }
   }
-  &__active-line {
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    height: 2px;
-    background-color: black;
-    transition: transform 0.4s ease, width 0.4s ease;
+
+  .default-tabs {
+    width: 85%;
+    position: relative;
+    margin: 0 auto;
+    &__item {
+      display: inline-block;
+      margin: 0 5px;
+      padding: 10px;
+      padding-bottom: 8px;
+      font-size: 16px;
+      letter-spacing: 0.8px;
+      color: #303030;
+      text-decoration: none;
+      border: none;
+      background-color: transparent;
+      border-bottom: 2px solid transparent;
+      cursor: pointer;
+      transition: all 0.25s;
+      &_active {
+        color: black;
+      }
+      &:hover {
+        border-bottom: 2px solid #c98811;
+        color: #c98811;
+      }
+      &:focus {
+        outline: none;
+        border-bottom: 2px solid #c98811;
+        color: #c98811;
+      }
+      &:first-child {
+        margin-left: 0;
+      }
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+    &__active-line {
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      height: 2px;
+      background-color: black;
+      transition: transform 0.4s ease, width 0.4s ease;
+    }
   }
-}
-.content {
-  margin-top: 30px;
-  font-size: 20px;
+  .content {
+    margin-top: 30px;
+    font-size: 20px;
+    width: 85%;
+    margin: auto;
+  }
 }
 </style>
