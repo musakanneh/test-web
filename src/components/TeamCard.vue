dc2e0f76d8ac5306e9bd7a49e67f1154027ce39c
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-22 23:28:28 +0200

    Updated homepage

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -2,63 +2,80 @@
   <body>
     <div class="team-and-boardmembers__cover">
       <main class="container">
-        <div>
-          <h1>BOARD</h1>
-          <section>
+        <h1>BOARD</h1>
+        <div class="row">
+          <div class="col-xs-6 col-md-3">
             <div>
               <img src="../assets/leb.png" alt="" />
               <TeamTag name="Dr. Lekumok Kironyi" position="Chairman" />
               <router-link to="/lekumok"> <p>Profile</p></router-link>
             </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
             <div>
               <img src="../assets/lilian.png" alt="" />
               <TeamTag name="Lilian Looloitai" position="Treasurer" />
               <router-link to="/lilian"> <p>Profile</p></router-link>
             </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
             <div>
               <img src="../assets/jop.png" alt="" />
               <TeamTag name="Jopha Peter Kakanyi" position="Member" />
               <router-link to="/jopha"> <p>Profile</p></router-link>
             </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
             <div>
               <img src="../assets/melubo.png" alt="" />
               <TeamTag name="Emanuel Melubo" position="Member" />
               <router-link to="/emanuel"> <p>Profile</p></router-link>
             </div>
-          </section>
+          </div>
         </div>
       </main>
 
       <h1>OUR TEAM</h1>
       <section class="team-member_section container">
-        <div>
-          <img src="../assets/marco.jpeg" alt="" />
-          <TeamTag name="Elias Nagol" position="Technical Advisor" />
-          <router-link to="/elias"> <p>Profile</p></router-link>
-        </div>
-        <div>
-          <img src="../assets/evel.png" alt="" />
-          <TeamTag name="Evelyn Kaney" position="Financial Manager" />
-          <router-link to="/evelyn"> <p>Profile</p></router-link>
-        </div>
-
-        <div>
-          <img src="../assets/marco.jpeg" alt="" />
-          <TeamTag
-            name="Isaac Ouma"
-            position="Conservation Enterprise Manager"
-          />
-          <router-link to="/isaac"> <p>Profile</p></router-link>
-        </div>
-        <div>
-          <img src="../assets/marco.jpeg" alt="" />
-          <TeamTag name="Marco Mathayo" position="Programs Manager" />
-          <router-link to="/marco"> <p>Profile</p></router-link>
-        </div>
-        <div>
-          <img src="../assets/yannick.png" alt="" />
-          <TeamTag name="Yannick Ndoinyo" position="Executive Director" />
-          <router-link to="/yannick"> <p>Profile</p></router-link>
+        <div class="row">
+          <div class="col-xs-6 col-md-3">
+            <div>
+              <img src="../assets/marco.jpeg" alt="" />
+              <TeamTag name="Elias Nagol" position="Technical Advisor" />
+              <router-link to="/elias"> <p>Profile</p></router-link>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <div>
+              <img src="../assets/evel.png" alt="" />
+              <TeamTag name="Evelyn Kaney" position="Financial Manager" />
+              <router-link to="/evelyn"> <p>Profile</p></router-link>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <div>
+              <img src="../assets/marco.jpeg" alt="" />
+              <TeamTag
+                name="Isaac Ouma"
+                position="Conservation Enterprise Manager"
+              />
+              <router-link to="/isaac"> <p>Profile</p></router-link>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <div>
+              <img src="../assets/marco.jpeg" alt="" />
+              <TeamTag name="Marco Mathayo" position="Programs Manager" />
+              <router-link to="/marco"> <p>Profile</p></router-link>
+            </div>
+          </div>
+          <div class="col-xs-6 col-md-3">
+            <img src="../assets/yannick.png" alt="" />
+            <div>
+              <TeamTag name="Yannick Ndoinyo" position="Executive Director" />
+              <router-link to="/yannick"> <p>Profile</p></router-link>
+            </div>
+          </div>
         </div>
       </section>
     </div>
@@ -79,82 +96,34 @@ export default {
 
 <style lang="scss" scoped>
 body {
-  padding: 1rem 0 0 0;
-
   .team-and-boardmembers__cover {
     background: #ddd;
     width: 100%;
     height: 100%;
     padding: 2rem 0 4rem 0;
+  }
 
-    h1 {
-      font-size: 1.5rem;
-      color: #303030 !important;
-      padding: 2rem 0;
-      text-align: center;
-    }
+  h1 {
+    font-size: 3rem !important;
+    padding: 6rem 0 4rem 0;
+    width: 100%;
+    margin: 0;
+    text-align: center;
   }
 
   .team-member_section {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-
-    div {
-      p {
-        text-align: center;
-      }
-    }
-
     img {
-      width: 80%;
-      margin: 0 1.25rem;
+      width: 70%;
+      margin-left: 15%;
       border-radius: 50rem;
-      border: 1px solid #707070;
     }
   }
 
   main {
-    position: relative;
-    padding: 3px 50px;
-    height: 100%;
-
     img {
       width: 70%;
       border-radius: 50rem;
-      border: 1px solid #707070;
-    }
-
-    h1 {
-      font-size: 1.5rem;
-      padding: 2rem 0;
-      text-align: center;
-    }
-
-    section {
-      justify-content: space-between;
-      width: 100%;
-      display: grid;
-      grid-template-columns: 1fr 1fr 1fr 1fr;
-
-      h3 {
-        margin: 0;
-      }
-
-      div {
-        text-align: center;
-
-        p {
-          font-size: 14px;
-          width: 80%;
-          padding: 0.5rem 0;
-          margin: auto;
-        }
-
-        a {
-          text-decoration: none;
-          font-size: 12px;
-        }
-      }
+      margin-left: 15%;
     }
   }
 }
