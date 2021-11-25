db64a929cabb92c48eb48f55c9ce469032c2ab87
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-11-25 15:21:58 +0200

    Updated homepage

--- src/components/OurFocus.vue
+++ src/components/OurFocus.vue
@@ -5,9 +5,7 @@
       <div class="row">
         <div class="col-xs-6 col-md-3">
           <div class="inner">
-            <h2>
-              11
-            </h2>
+            <h2>11</h2>
             <p>regions</p>
             <br /><br />
           </div>
@@ -15,9 +13,7 @@
         </div>
         <div class="col-xs-6 col-md-3">
           <div class="inner">
-            <h2>
-              300
-            </h2>
+            <h2>300</h2>
             <p>villages</p>
             <br /><br />
           </div>
@@ -25,9 +21,7 @@
         </div>
         <div class="col-xs-6 col-md-3">
           <div class="inner">
-            <h3>
-              12 Million IPLCs
-            </h3>
+            <h3>12 Million IPLCs</h3>
             <p>
               that live and share their lands with wildlife in critical and
               globally important ecosystem hotspots
@@ -37,9 +31,7 @@
         </div>
         <div class="col-xs-6 col-md-3">
           <div class="inner">
-            <h3>
-              Culturally diverse communities
-            </h3>
+            <h3>Culturally diverse communities</h3>
             <p>
               including pastoralists, hunters-gatherers and agro-pastoralist
               communities in Tanzania.
--- src/views/Emanuel.vue
+++ src/views/Emanuel.vue
@@ -4,7 +4,7 @@
     <section class="container">
       <div class="profile-content__cover">
          <img
-          src="../assets/marco.jpeg"
+          src="../assets/melubo.png"
           class="img-responsive img-circle"
           alt="Responsive image"
         />
