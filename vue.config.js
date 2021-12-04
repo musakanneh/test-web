03a395c6aab74b90b1b92db249c328c7ddb66d01
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-12-04 22:46:50 +0200

    SEO Update

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,43 @@
+// module.exports = {
+// chainWebpack: config => {
+//     if (process.env.NODE_ENV === 'development') {
+//         config
+//             .output
+//             .filename('[name].[hash].js')
+//             .end()
+//     } 
+// }
+// pluginOptions: {
+//     prerenderSpa: {
+//         registry: undefined,
+//         renderRoutes: [
+//             '/',
+//             '/about',
+//         ],
+//         useRenderEvent: true,
+//         onlyProduction: true,
+
+//         headless: false, // <- this could also be inside the customRendererConfig
+//         customRendererConfig:
+//         {
+//             args: ["--auto-open-devtools-for-tabs"]
+//         }
+//     }
+// }
+const path = require('path')
+const PrerenderSPAPlugin = require('prerender-spa-plugin')
+
 module.exports = {
-    chainWebpack: config => {
-        if (process.env.NODE_ENV === 'development') {
-            config
-                .output
-                .filename('[name].[hash].js')
-                .end()
-        }
+    configureWebpack: {
+        plugins: process.env.NODE_ENV === 'production' ? [
+            new PrerenderSPAPlugin({
+                // Required - The path to the webpack-outputted app to prerender.
+                staticDir: path.join(__dirname, 'dist'),
+                // Required - Routes to render.
+                routes: ['/', '/about', '/work', '/approach', '/projects', '/documents'],
+            })
+        ] : []
     }
 }
+
+// }
