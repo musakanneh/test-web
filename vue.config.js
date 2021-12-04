c3590f6adedc51983eeae6b811141aa146ebf6dd
Musa Selekie Kanneh <musas.kanneh@gmail.com>
2021-12-04 22:55:26 +0200

    SEO Update

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,12 @@
-// module.exports = {
-// chainWebpack: config => {
-//     if (process.env.NODE_ENV === 'development') {
-//         config
-//             .output
-//             .filename('[name].[hash].js')
-//             .end()
-//     } 
-// }
+module.exports = {
+chainWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+        config
+            .output
+            .filename('[name].[hash].js')
+            .end()
+    } 
+}
 // pluginOptions: {
 //     prerenderSpa: {
 //         registry: undefined,
@@ -24,20 +24,20 @@
 //         }
 //     }
 // }
-const path = require('path')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
+// const path = require('path')
+// const PrerenderSPAPlugin = require('prerender-spa-plugin')
 
-module.exports = {
-    configureWebpack: {
-        plugins: process.env.NODE_ENV === 'production' ? [
-            new PrerenderSPAPlugin({
-                // Required - The path to the webpack-outputted app to prerender.
-                staticDir: path.join(__dirname, 'dist'),
-                // Required - Routes to render.
-                routes: ['/', '/about', '/work', '/approach', '/projects', '/documents'],
-            })
-        ] : []
-    }
-}
-
+// module.exports = {
+//     configureWebpack: {
+//         plugins: process.env.NODE_ENV === 'production' ? [
+//             new PrerenderSPAPlugin({
+//                 // Required - The path to the webpack-outputted app to prerender.
+//                 staticDir: path.join(__dirname, 'dist'),
+//                 // Required - Routes to render.
+//                 routes: ['/', '/about', '/work', '/approach', '/projects', '/documents'],
+//             })
+//         ] : []
+//     }
 // }
+
+}
