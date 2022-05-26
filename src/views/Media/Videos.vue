baf8a03b8d951f738a73b0e0351112130db4d3c9
Ğ3ñě$ <genessambayena@gmail.com>
2022-05-26 19:01:45 +0300

    add video to video section

--- src/views/Media/Videos.vue
+++ src/views/Media/Videos.vue
@@ -1,10 +1,19 @@
 <template>
-  <div class="height_and_weight__global">
-    <main class="container">
-      <br /><br /><br />
-      <h3>No available videos yet!</h3>
-    </main>
-  </div>
+	<div class="height_and_weight__global">
+		<main class="container animate_1">
+			<div class="video-container">
+				<iframe
+					width="560"
+					height="315"
+					src="https://www.youtube.com/embed/CbrTZdxe9JQ?controls=0"
+					title="YouTube video player"
+					frameborder="0"
+					allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+					allowfullscreen
+				></iframe>
+			</div>
+		</main>
+	</div>
 </template>
 
 <script>
@@ -13,10 +22,13 @@ export default {};
 
 <style lang="scss" scoped>
 @media only screen and (max-width: 900px) {
-  body {
-    main {
-      margin-top: -8rem !important;
-    }
-  }
+	body {
+		main {
+			margin-top: -8rem !important;
+		}
+	}
+}
+.video-container {
+	padding-top: 9rem !important;
 }
 </style>
