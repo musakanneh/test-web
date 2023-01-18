d0f6a0d1230f4c96bf750c51b8ee6788dfe27ea7
Ğ3ñě$ <genessambayena@gmail.com>
2023-01-18 12:21:36 +0300

    Update Projects.vue

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,4 +1,5 @@
 <template>
+
   <body>
     <div class="global__height"></div>
     <div class="container animate_1">
@@ -7,38 +8,119 @@
       <div class="row">
         <div class="col-xs-6 col-md-12 border__">
           <h3>
-            Co-developing Data Technology for Inclusive Conservation in the
-            Eastern Serengeti.
+            Participatory Development of Climate Resilience and Vulnerability Protocols in Ngorongoro District, Tanzania
           </h3>
           <div class="content__text">
             <p>
-              This is a 1-year project with the aim of co-developing together
-              with the local communities, a mobile app and data collection and
-              management protocols. This will serve the interests of the
-              communities for more research to inform both the local indigenous
-              people on their wildlife monitoring and livestock grazing
-              strategies, and protected areas management. This project is funded
-              by the Centre for Indigenous Conservation and Development
-              Alternatives (CICADA at the University of McGill, Canada.
+              The climate change and vulnerability impacts continue to affect the indigenous communities in
+              Tanzania without a tool or mechanism for regular assessment and creating awareness for solutions for
+              these effects. This project aims at developing a Climate Change Adaptation Toolkit (CCA Toolkit).
+              TEST will create a baseline through this Toolkit for climate change vulnerability assessments and
+              adaptation as part of a future funded project on “Resilience to Climate Change”. The project aims to
+              build the capacity of Maasai communities and stakeholders in Ngorongoro district to adopt and apply
+              the assessment in other divisions, and to be compliant with the Toolkit for Ecosystem Service Site-based
+              Assessment &quot;TESSA Toolkit&quot; for worldwide assessment and adaptation. <br /><br />
+              Economies in the traditional ecosystem are highly dependent on natural resources, meaning that
+              recurring climate change patterns such as recurrent droughts, erratic rainfall patterns and floods will
+              continue to negatively impact livelihoods and community assets. Knowing that pastoral communities
+              are at the central concern of climate change, TEST is implementing Participatory Development of
+              Climate Resilience and Vulnerability Protocols in Ngorongoro District, Tanzania. This program
+              operates in two divisions, Loliondo and Sale respectively. This project is funded by Australian
+              Volunteers International (AVI) through Impact Funds.
             </p>
           </div>
         </div>
         <div class="col-xs-6 col-md-12 border__">
           <h3>
-            Twiga Wetu: Masai Giraffe Conservation Outside Protected Areas – the
-            Role of Indigenous Knowledge and Traditional Systems.
+            Preservation of Indigenous Knowledge
           </h3>
           <div class="content__text">
             <p>
-              This project is a 1-year project (Oct. 2020 – Sept. 2021) funded
-              by Giraffe Conservation Foundation
-              <a href="www.giraffeconservation.org"
-                >(www.giraffeconservation.org)</a
-              >. The aim of this project is to revitalize indigenous
-              conservation of Masai giraffe in the eastern Serengeti Ecosystem.
-              This will serve the interests of the local communities to be
-              involved in the conservation and monitoring of giraffe linked with
-              livestock grazing strategies outside protected areas.
+              The <b>objective</b> of this project is to promote indigenous practices and cultural values that enhance sustainable
+              natural resources management. Indigenous knowledge is the unique knowledge intrinsic to a particular
+              culture or society. In most cases this type of knowledge is not written and or stored by use of any form
+              of modern techniques. This knowledge is generated and transmitted by communities, over time, to cope
+              with their own agro-ecological and socio-economic environments. <br /><br />
+              TEST will implement this project through documentation and production in print and through video the best
+              practices of the Maasai people that contribute significantly to ecological and cultural resilience. TEST will
+              conduct interviews with elders, women, and youth on rituals of the indigenous Maasai people and compile
+              responses in the form of a video documentary. The final documentary will be disseminated to the public
+              through Loliondo FM Community radio. TEST will use the document and video to create awareness to the local
+              community and the government of the importance of the indigenous best practices that support
+              environmental/ecology preservation that contributes to indigenous livelihoods and conservation. <br /><br />
+              TEST has embarked on journey to safeguard the Maasai indigenous knowledge thereby started by
+              producing a documentary on indigenous knowledge and cultural values that enhance sustainable Natural
+              Resources Management, a project implemented in Longido district, Arusha region. This project is
+              funded by Global Greengrants Fund (GGF)
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-12 border__">
+          <h3>
+            Community Art Space (CAS)
+          </h3>
+          <div class="content__text">
+            <p>
+              CAS provides a platform to enhance social interaction and engagement and facilitate economic
+              revitalization. This is social enterprise that combines two vital elements of community development –
+              income generating activities and capacity buildings. Youth and women come together to learn skills to
+              diversify economy. TEST has been requested to support this project in its initial stages of growth
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-12 border__">
+          <h3>A community radio known as Loliondo FM (107.7 MHz)</h3>
+          <div class="content__text">
+            <p>
+              <b> Loliondo Community Radio - </b> situated in Wasso (Ngorongoro district
+              headquarters) is the only radio in the entire district of Ngorongoro,
+              serving a population of 200,000 people in 4 districts (Ngorongoro, Tarime,
+              Serengeti and Narok Kenya). The community radio is an amplifier of community
+              voices and a change agent for all stakeholders. Above all, it is a channel
+              for airing TEST programs through radio shows. <br /><br />
+              The fundamental functions of Loliondo Community Radio are: <br />
+            <ul>
+              <li> To reflect and promote local identity, character, and culture by focusing principally on local content.</li>
+              <li> To create a diversity of voices and opinions on the air through its openness to
+                participation from all sectors.</li>
+              <li> To encourage open dialogue and democratic process by providing an independent platform for interactive discussion about matters and decisions of importance to the community </li>
+              <li> To promote social change and development. </li>
+              <li> To promote good governance and civil society by playing a community watchdog role that makes local authorities and politicians more conscious of their public responsibilities.</li>
+            </ul>
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-12 border__">
+          <h3>
+            Co-developing Data Technology for Inclusive Conservation in the Eastern
+            Serengeti.
+          </h3>
+          <div class="content__text">
+            <p>
+              This is a 1-year project with the aim of co-developing together with the
+              local communities, a mobile app and data collection and management
+              protocols. This will serve the interests of the communities for more
+              research to inform both the local indigenous people on their wildlife
+              monitoring and livestock grazing strategies, and protected areas management.
+              This project is funded by the Centre for Indigenous Conservation and
+              Development Alternatives (CICADA at the University of McGill, Canada).
+            </p>
+          </div>
+        </div>
+        <div class="col-xs-6 col-md-12 border__">
+          <h3>
+            Twiga Wetu: Masai Giraffe Conservation Outside Protected Areas – the Role of
+            Indigenous Knowledge and Traditional Systems.
+          </h3>
+          <div class="content__text">
+            <p>
+              This project is a 1-year project (Oct. 2020 – Sept. 2021) funded by Giraffe
+              Conservation Foundation
+              <a href="www.giraffeconservation.org">(www.giraffeconservation.org)</a>. The
+              aim of this project is to revitalize indigenous conservation of Masai
+              giraffe in the eastern Serengeti Ecosystem. This will serve the interests of
+              the local communities to be involved in the conservation and monitoring of
+              giraffe linked with livestock grazing strategies outside protected areas.
             </p>
           </div>
         </div>
@@ -58,18 +140,21 @@ export default {
   .global__height {
     display: none;
   }
+
   .container {
     h4 {
       padding-top: 3rem;
       margin-bottom: -2rem !important;
     }
   }
+
   .border__ {
     display: block !important;
 
     h3 {
       padding: 2rem 0 1rem 0;
     }
+
     p {
       margin: 0 !important;
       padding: 0.5rem 0;
